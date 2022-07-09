9738cc0e72872b4242878d5336d7b9ba8effa58c
yoongyuanzhi <81006068+yoongyuanzhi@users.noreply.github.com>
2022-07-09 21:29:41 +0800

    Add files via upload

---
+++ socialData.py
@@ -0,0 +1,76 @@
+social_organisations={
+
+    'n':
+    '''Bright Vision Hospital \n
+Description: celebrate birthdays and give haircuts to elderly patients \n
+Requirement: NIL \n
+Location: 5 Lorong Napiri, Singapore 547530 \n
+Sign-Up Form: https://www.giving.sg/manage-volunteers?orgId=3108708 \n
+Website: https://www.singhealth.com.sg/SCH/our-hospitals/bright-vision-hospital/about-bvh \n
+
+    Thye Hua Kwan Hospital \n
+Description: Cheer seniors up as they recover from treatments \n
+Requirement: NIL \n
+Location: 17 Ang Mo Kio Ave 9, Singapore 569766 \n
+Sign-Up Form: Email volunteer@amkh.org.sg \n
+Website: http://www.amkh.org.sg/ ''' ,
+
+    'ne': 
+    '''Singapore Anti Narcotics Association \n
+Description: Join in the fight against drugs, volunteer roles include being a service facilitator, para counsellor, outreach ambassador, photographer or videographer \n
+Requirement: NIL \n
+Location: 2 Sengkang Square, Sengkang Community Hub, #05-01, Singapore 545025 \n
+Sign-Up Form: https://www.sana.org.sg/index.php/volunteer-3// \n
+Website: https://www.sana.org.sg/ \n
+
+    Institute of Mental Health \n
+Description: Encourage mental patients by engaging them with fun activities \n
+Requirement: NIL \n
+Location: Buangkok Green Medical Park, 10 Buangkok View, Singapore 539747 \n
+Sign-Up Form: https://www.sana.org.sg/index.php/volunteer-3// \n
+Website: https://www.sana.org.sg/ \n ''' ,
+
+    'central': 
+    '''Singapore Association for the Deaf \n
+Description: Engage those with hearing impairments \n
+Requirement: NIL \n
+Location: 227 Mountbatten Road, Singapore 397998 \n
+Sign-Up Form: http://sadeaf.org.sg/join-us/be-our-volunteer/ \n
+Website: https://sadeaf.org.sg/ \n
+
+    St. Hilda’s Community Services \n
+Description: Engage with elderly \n
+Requirement: NIL \n
+Location: Blk 10 Jalan Batu #01-06 Singapore 431010 \n
+Sign-Up Form: http://sthildascsc.org.sg/?page_id=588 \n
+Website: http://sthildascsc.org.sg/ \n ''',
+
+    'east':
+    '''Willing Hearts \n
+Description: Help in food preparation for the needy \n
+Requirement: NIL \n
+Location: 11 Jalan Ubi Blk 6, #01-51, Kembangan , Chai Chee Community Hub, Singapore 409074 \n
+Sign-Up Form: https://willinghearts.eventbrite.com/ \n
+Website: https://www.willinghearts.org.sg/ \n
+
+    Yayasan Mendaki \n
+Description: Mentor and befriend children \n
+Requirement: NIL \n
+Location: 51 Kee Sun Avenue Singapore 457056 \n
+Sign-Up Form: https://linktr.ee/volunteermendaki \n
+Website: https://www.mendaki.org.sg/en/ \n  ''' ,
+
+    'west':
+    '''Grace Orchard School\n
+Description: Help the intellectually challenged learn valuable skills \n
+Requirement: NIL \n
+Location: 6A Jurong West St 52, Singapore 649297 \n
+Sign-Up Form: https://go.edu.sg/volunteers-opp/ \n
+Website: https://go.edu.sg/ \n
+
+    KidsSTART Group Helpers\n
+Description: Engage children at playgroups sessions\n
+Requirement: At least 5/20 sessions \n
+Location: 804 Woodlands Street 81 #01-37, Woodlands Centre Singapore 730804 \n
+Sign-Up Form & website : https://bit.ly/3OVD3LI\n '''
+}
