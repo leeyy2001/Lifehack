d9b90de34951e03a9c8cdbdd99727c2d298f2563
yoongyuanzhi <81006068+yoongyuanzhi@users.noreply.github.com>
2022-07-10 10:26:18 +0800

    Add files via upload

--- environmentData.py
+++ environmentData.py
@@ -1,67 +1,72 @@
 environmental_organisations={
 
     'n':
-    '''GUI Volunteer Programme \n
+    '''
+    1.GUI Volunteer Programme \n
 Description: Engage in fieldwork so that more green spaces are created for people to connect with nature \n
 Location: 91 LORONG CHENCHARU Singapore 769201\n
 Sign-Up and website: https://bit.ly/3NTd6LA \n
 
-    Rooftop Gardening \n
+    2.Rooftop Gardening \n
 Description: Care and harvest crops grown at the rooftop of Yishun Community Hospital \n
 Location: 2 YISHUN CENTRAL 2 Singapore 768024 \n
 Sign-Up and website: https://bit.ly/3PktjKV \n ''' ,
 
     'ne': 
-    '''Community Aquaponics Project \n
+    '''
+    1.Community Aquaponics Project \n
 Description: This project uses a de-coupled aquaponics farming system to benefit the low-income residents of Ang Mo Kio GRC and seeks to play a part in Singapore's 30 by 30 food production target. \n
 Location: ANG MO KIO INDUSTRIAL PARK 2 Singapore 569549 \n
 Sign-Up and website: https://bit.ly/3ySV9Z9 \n
 
-    WWFS Conservation Fund \n
+    2.WWFS Conservation Fund \n
 Description: Help detect and report illegal wildlife product postings on digital platforms \n
 Location: Virtual \n
 Sign-Up and website: https://bit.ly/3nT3sOp \n ''' ,
 
     'central': 
-    '''MAD Garden \n
+    '''
+    1.MAD Garden \n
 Description: Family gardening \n
 Location: 10 WINSTEDT ROAD Singapore 227977 \n
 Sign-Up and website: https://bit.ly/3uwi367 \n
 
-    WWFS Conservation Fund \n
+    2.WWFS Conservation Fund \n
 Description: Help detect and report illegal wildlife product postings on digital platforms \n
 Location: Virtual \n
 Sign-Up and website: https://bit.ly/3nT3sOp \n ''',
 
     'east':
-    '''Sustainability@Tampines Park \n
+    '''
+    1.Sustainability@Tampines Park \n
 Description: Volunteer help to use larvae to upcycle food waste from nearby residents and schools. Volunteers are rewarded with "Green currency" which can be used to off-setting the cost of purchase for food and programs \n
 Location: 144 TAMPINES STREET 12 Singapore 521144 \n
 Sign-Up and website: https://bit.ly/3PiJfNM \n
 
-    SeasTheDay \n
+    2.SeasTheDay \n
 Description: Beach Clean Up \n
 Location: East Coast Park \n
 Sign-Up and website: https://bit.ly/3NUxyvv \n ''' ,
 
     'west':
-    '''The Sustainability Project \n
+    '''
+    1.The Sustainability Project \n
 Description: Aims to encourage and inspire the adoption of a low or zero waste lifestyle in your life. \n
 Location: Island Wide \n
 Sign-Up: http://www.zerowastesg.com/volunteer/ \n
 Website: https://www.zerowastesg.com/ \n
 
-    Zero Waste SG \n
+    2.Zero Waste SG \n
 Description: Zero Waste SG is a charity, non-governmental organisation driving zero waste action in Singapore through education and advocacy. \n
 Location: Island Wide \n
 Sign-Up and website:  https://thesustainabilityproject.life/shop/tree-hugging-team/ \n
 
-    BlockWalk \n
+    3.BlockWalk \n
 Description: Picking up litter at our own neighbourhoods \n
 Location: Island Wide, respective neighbourhood \n
 Sign-Up and website: https://bit.ly/3nS7VAI \n
 
-    WWFS Conservation Fund \n
+    4.WWFS Conservation Fund \n
 Description: Help detect and report illegal wildlife product postings on digital platforms \n
 Location: Virtual \n
 Sign-Up and website: https://bit.ly/3nT3sOp \n '''
